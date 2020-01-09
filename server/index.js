2389a24686925a58ea58e89a09e964f220b3fd24
Yoon_Koh <yk1803@nyu.edu>
2020-01-09 15:58:14 +0900

    first commit

---
+++ server/index.js
@@ -0,0 +1,13 @@
+// import express from 'express';
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send({ hi: 'there ' });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT);
+3;
